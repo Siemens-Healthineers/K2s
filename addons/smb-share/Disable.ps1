ae5c8b97399f9b9bfed60adf40a4dd9a1d92c20a
Jacob Grieger <jacob.grieger@siemens-healthineers.com>
2024-02-05 08:49:34 +0100

    #42 fix(smb-share addon): message instead of error when system is stopped

--- addons/smb-share/Disable.ps1
+++ addons/smb-share/Disable.ps1
@@ -16,24 +16,55 @@ Param(
     [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
     [switch] $ShowLogs = $false,
     [parameter(Mandatory = $false, HelpMessage = 'Skips user confirmation if set to true')]
-    [switch] $Force = $false
+    [switch] $Force = $false,
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType
 )
 &$PSScriptRoot\..\..\smallsetup\common\GlobalVariables.ps1
 . $PSScriptRoot\..\..\smallsetup\common\GlobalFunctions.ps1
 
-Import-Module "$PSScriptRoot/../../smallsetup/ps-modules/log/log.module.psm1"
+$logModule = "$PSScriptRoot/../../smallsetup/ps-modules/log/log.module.psm1"
+$cliMessagesModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/cli-messages/cli-messages.module.psm1"
+$smbShareModule = "$PSScriptRoot\module\Smb-share.module.psm1"
+
+Import-Module $logModule, $cliMessagesModule, $smbShareModule
+
 Initialize-Logging -ShowLogs:$ShowLogs
 
 if ($Force -ne $true) {
     $answer = Read-Host 'WARNING: This DELETES ALL DATA of the shared SMB folder. Continue? (y/N)'
     if ($answer -ne 'y') {
         Write-Log 'Disabling cancelled.' -Console
+        if ($EncodeStructuredOutput -eq $true) {
+            Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+        }
         return
     }    
 }
 
-Import-Module "$PSScriptRoot\module\Smb-share.module.psm1"
-
 Write-Log "Disabling addon '$addonName'.."
 
-Disable-SmbShare
+$result = Disable-SmbShare
+
+if ($result.Error) {
+    if ($result.Error -eq 'already-disabled') {
+        if ($EncodeStructuredOutput -eq $true) {
+            Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+        }
+        return
+    }
+
+    if ($EncodeStructuredOutput -eq $true) {
+        Send-ToCli -MessageType $MessageType -Message @{Error = $result.Error }
+        return
+    }
+
+    Write-Log $result.Error -Error
+    exit 1
+}
+
+if ($EncodeStructuredOutput -eq $true) {
+    Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+}
