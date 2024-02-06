7f7600db70dfe80fc3cfddd78488a99ea05901e2
Joachim Kist <155652202+achimkist@users.noreply.github.com>
2024-02-06 18:23:11 +0100

    Add user confirmation to k2s reset network (#107)

--- smallsetup/helpers/ResetNetwork.ps1
+++ smallsetup/helpers/ResetNetwork.ps1
@@ -4,15 +4,47 @@
 
 #Requires -RunAsAdministrator
 
+Param(
+    [parameter(Mandatory = $false, HelpMessage = 'Force deleting network settings')]
+    [switch] $Force = $false,
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType,
+    [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
+    [switch] $ShowLogs = $false
+)
+
 &$PSScriptRoot\..\common\GlobalVariables.ps1
 
-Write-Output "Removing HNS Network"
+$logModule = "$PSScriptRoot\..\ps-modules\log\log.module.psm1"
+$cliMessagesModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/cli-messages/cli-messages.module.psm1"
+Import-Module $logModule, $cliMessagesModule
+
+Initialize-Logging -ShowLogs:$ShowLogs
+
+if ($Force -ne $true) {
+    $answer = Read-Host 'WARNING: THIS DELETES ALL NETWORK SETTINGS. Continue? (y/N)'
+    if ($answer -ne 'y') {
+        Write-Log 'Resetting network cancelled.'
+        if ($EncodeStructuredOutput -eq $true) {
+            Send-ToCli -MessageType $MessageType -Message @{Error = "Network reset cancelled." }
+        }
+        return
+    }    
+}
+
+Write-Log "Removing HNS Network" 
 Import-Module "$global:KubernetesPath\smallsetup\LoopbackAdapter.psm1" -Force
 Get-NetAdapter | Where-Object InterfaceDescription -like 'Microsoft KM-TEST Loopback Adapter*' | ForEach-Object { Remove-LoopbackAdapter -Name $_.Name -DevConExe $global:DevconExe }
 
 Get-HnsNetwork | Remove-HnsNetwork
-Write-Output "Delete Network Configuration"
+Write-Log "Delete Network Configuration"
 netcfg -d
 
-Write-Output "Restarting Computer"
+if ($EncodeStructuredOutput -eq $true) {
+    Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+}
+
+Write-Log "Restart computer now?" -Console
 Restart-Computer -Confirm
