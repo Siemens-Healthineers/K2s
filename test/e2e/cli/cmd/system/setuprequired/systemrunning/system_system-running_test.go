a52422280f13a20217f75a4f2e6296530b767574
Jacob Grieger <jacob.grieger@siemens-healthineers.com>
2024-02-13 12:33:33 +0100

    148 fix([CLI] system scp): message instead of error when system is running

---
+++ test/e2e/cli/cmd/system/setuprequired/systemrunning/system_system-running_test.go
@@ -0,0 +1,146 @@
+// SPDX-FileCopyrightText:  © 2023 Siemens Healthcare GmbH
+// SPDX-License-Identifier:   MIT
+package systemrunning
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k2sTest/framework"
+)
+
+var suite *framework.K2sTestSuite
+
+func TestSystem(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "system CLI Commands Acceptance Tests", Label("cli", "system", "scp", "m", "acceptance", "setup-required", "system-running"))
+}
+
+var _ = BeforeSuite(func(ctx context.Context) {
+	suite = framework.Setup(ctx, framework.SystemMustBeRunning, framework.ClusterTestStepPollInterval(100*time.Millisecond))
+})
+
+var _ = AfterSuite(func(ctx context.Context) {
+	suite.TearDown(ctx)
+})
+
+var _ = Describe("system", func() {
+	Describe("scp", func() {
+		Describe("m", func() {
+			const testFileName = "system_scp_m_test.txt"
+			const tempFileContent = "scp-m-test\n"
+			var localTempFilePath string
+			var remoteTempFilePath string
+
+			BeforeEach(func() {
+				tempDir := GinkgoT().TempDir()
+				localTempFilePath = filepath.Join(tempDir, testFileName)
+				remoteTempFilePath = fmt.Sprintf("/tmp/%s", testFileName)
+
+				Expect(os.WriteFile(localTempFilePath, []byte(tempFileContent), fs.ModePerm)).To(Succeed())
+
+				GinkgoWriter.Println("Test file <", localTempFilePath, "> written")
+			})
+
+			AfterEach(func(ctx context.Context) {
+				var localErr error
+
+				_, err := os.Stat(localTempFilePath)
+				if err == nil {
+					GinkgoWriter.Println("Deleting <", localTempFilePath, ">..")
+
+					Expect(os.Remove(localTempFilePath)).To(Succeed())
+				} else {
+					if os.IsNotExist(err) {
+						GinkgoWriter.Println("Test file <", localTempFilePath, "> does not exist anymore")
+						return
+					}
+					localErr = err
+				}
+
+				GinkgoWriter.Println("Deleting <", remoteTempFilePath, ">..")
+
+				suite.K2sCli().Run(ctx, "system", "ssh", "m", "--", fmt.Sprintf("rm -f %s", remoteTempFilePath))
+
+				Expect(localErr).ToNot(HaveOccurred())
+			})
+
+			It("copies a file from host to Linux node", func(ctx context.Context) {
+				suite.K2sCli().Run(ctx, "system", "scp", "m", localTempFilePath, "/tmp")
+
+				output := suite.K2sCli().Run(ctx, "system", "ssh", "m", "--", fmt.Sprintf("cat %s", remoteTempFilePath))
+
+				Expect(output).To(Equal(tempFileContent))
+			})
+		})
+
+		Describe("m reverse", func() {
+			const testFileName = "system_scp_m_reverse_test.txt"
+			const tempFileContent = "scp-m-reverse-test"
+			var localTempFilePath string
+			var remoteTempFilePath string
+
+			BeforeEach(func(ctx context.Context) {
+				tempDir := GinkgoT().TempDir()
+				localTempFilePath = filepath.Join(tempDir, testFileName)
+				remoteTempFilePath = fmt.Sprintf("/tmp/%s", testFileName)
+
+				suite.K2sCli().Run(ctx, "system", "ssh", "m", "--", fmt.Sprintf("echo %s >> %s", tempFileContent, remoteTempFilePath))
+
+				GinkgoWriter.Println("Test file <", remoteTempFilePath, "> written")
+			})
+
+			AfterEach(func(ctx context.Context) {
+				var localErr error
+
+				_, err := os.Stat(localTempFilePath)
+				if err == nil {
+					GinkgoWriter.Println("Deleting <", localTempFilePath, ">..")
+
+					Expect(os.Remove(localTempFilePath)).To(Succeed())
+				} else {
+					if os.IsNotExist(err) {
+						GinkgoWriter.Println("Test file <", localTempFilePath, "> does not exist anymore")
+						return
+					}
+					localErr = err
+				}
+
+				GinkgoWriter.Println("Deleting <", remoteTempFilePath, ">..")
+
+				suite.K2sCli().Run(ctx, "system", "ssh", "m", "--", fmt.Sprintf("rm -f %s", remoteTempFilePath))
+
+				Expect(localErr).ToNot(HaveOccurred())
+			})
+
+			It("copies a file from Linux node to host", func(ctx context.Context) {
+				suite.K2sCli().Run(ctx, "system", "scp", "m", remoteTempFilePath, localTempFilePath, "-r")
+
+				data, err := os.ReadFile(localTempFilePath)
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(string(data)).To(Equal(fmt.Sprintf("%s\n", tempFileContent)))
+			})
+		})
+
+		Describe("w", func() {
+			It("copies a file from host to Windows node", func() {
+				Skip("test to be implemented")
+			})
+		})
+
+		Describe("w reverse", func() {
+			It("copies a file from Windows node to host", func() {
+				Skip("test to be implemented")
+			})
+		})
+	})
+})
---
+++ test/e2e/cli/cmd/system/setuprequired/systemstopped/system_system-stopped_test.go
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText:  © 2023 Siemens Healthcare GmbH
+// SPDX-License-Identifier:   MIT
+package systemstopped
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k2sTest/framework"
+)
+
+var suite *framework.K2sTestSuite
+
+func TestSystem(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "system CLI Commands Acceptance Tests", Label("cli", "system", "scp", "m", "w", "acceptance", "setup-required", "system-stopped"))
+}
+
+var _ = BeforeSuite(func(ctx context.Context) {
+	suite = framework.Setup(ctx, framework.SystemMustBeStopped, framework.ClusterTestStepPollInterval(100*time.Millisecond))
+})
+
+var _ = AfterSuite(func(ctx context.Context) {
+	suite.TearDown(ctx)
+})
+
+var _ = Describe("system", func() {
+	DescribeTable("print system-not-running message",
+		func(ctx context.Context, args ...string) {
+			output := suite.K2sCli().Run(ctx, args...)
+
+			Expect(output).To(ContainSubstring("not running"))
+		},
+		Entry("scp m", "system", "scp", "m", "a1", "a2"),
+		Entry("scp w", "system", "scp", "w", "a1", "a2"),
+		// Entry("ssh m", "system", "ssh", "m"),
+		// Entry("ssh w", "system", "ssh", "w"),
+	)
+})
