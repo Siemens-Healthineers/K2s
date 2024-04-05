64e82bcb337621328e5c2e2a339be050a7149235
Joachim Kist <joachim.kist@siemens-healthineers.com>
2024-04-05 14:49:06 +0200

    start refactoring

--- k2s/internal/powershell/exec.go
+++ k2s/internal/powershell/exec.go
@@ -11,10 +11,16 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"math"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
+	"github.com/go-cmd/cmd"
+	"github.com/pterm/pterm"
+	"github.com/siemens-healthineers/k2s/internal/host"
+	"github.com/siemens-healthineers/k2s/internal/logging"
 	"github.com/siemens-healthineers/k2s/internal/powershell/decode"
 )
 
@@ -61,6 +67,10 @@ func ExecutePsWithStructuredResult[T any](psScriptPath string, targetType string
 	}
 
 	cmdString := buildCmdString(psScriptPath, targetType, additionalParams...)
+	cmdString, err = prepareExecScript(cmdString)
+	if err != nil {
+		return v, err
+	}
 
 	slog.Debug("PS command created", "command", cmdString)
 
@@ -227,4 +237,109 @@ func convertToResult[T any](messages []message) (v T, err error) {
 	slog.Info("Message unmarshalled")
 
 	return
+}
+
+func ExecutePs(script string, psVersion PowerShellVersion) (time.Duration, error) {
+	if psVersion == "" {
+		return 0, errors.New("PowerShell version not specified")
+	}
+
+	psCmd := Ps5CmdName
+	cmdArg := ""
+	if psVersion == PowerShellV7 {
+		psCmd = Ps7CmdName
+		cmdArg = "-Command"
+
+		slog.Info("Switching to PowerShell 7 command syntax")
+
+		if err := AssertPowerShellV7Installed(); err != nil {
+			return 0, err
+		}
+	}
+
+	cmdOptions := cmd.Options{
+		Buffered:   false,
+		Streaming:  true,
+		BeforeExec: []func(cmd *exec.Cmd){setStdin},
+	}
+
+	wrapperScript, err := prepareExecScript(script)
+	cmdRun := cmd.NewCmdOptions(cmdOptions, string(psCmd), cmdArg, wrapperScript)
+	doneChan := make(chan struct{})
+	errorLineBuffer, err := logging.NewLogBuffer(logging.BufferConfig{
+		Limit: 100,
+		FlushFunc: func(buffer []string) {
+			slog.Error("Flushing error lines", "count", len(buffer), "lines", buffer)
+		},
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	go readStdChannels(cmdRun, doneChan, errorLineBuffer.Log)
+
+	statusChan := cmdRun.Start()
+	finalStatus := <-statusChan
+	<-doneChan
+
+	errorLineBuffer.Flush()
+
+	if finalStatus.Exit != 0 {
+		return 0, fmt.Errorf("command execution failed, see log output above. Error: exit code %d", finalStatus.Exit)
+	}
+
+	seconds := math.Round(finalStatus.Runtime)
+	duration := time.Second * time.Duration(int(seconds))
+
+	return duration, nil
+}
+
+// TODO: merge/consolidate with k2s\internal\powershell\exec.go
+func readStdChannels(cmdRun *cmd.Cmd, doneChan chan struct{}, logErrFunc func(line string)) {
+	defer close(doneChan)
+
+	// Done when both channels have been closed
+	// https://dave.cheney.net/2013/04/30/curious-channels
+	for cmdRun.Stdout != nil || cmdRun.Stderr != nil {
+		select {
+		case line, open := <-cmdRun.Stdout:
+			if !open {
+				cmdRun.Stdout = nil
+				continue
+			}
+			if len(line) > 0 {
+				pterm.Printfln("⏳ %s", line)
+			}
+		case line, open := <-cmdRun.Stderr:
+			if !open {
+				cmdRun.Stderr = nil
+				continue
+			}
+			if len(line) > 0 {
+				logErrFunc(line)
+				pterm.Printfln("⏳ %s", pterm.Yellow(line))
+			}
+		}
+	}
+}
+
+func setStdin(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+}
+
+func prepareExecScript(script string) (string, error) {
+	slog.Debug("Execution script", "script", script)
+	wrapperScript := ""
+
+	installDir, err := host.ExecutableDir()
+	if err != nil {
+		return "", err
+	}
+
+	wrapperScript = ("&'" + installDir + "\\lib\\scripts\\k2s\\base\\" + "Invoke-ExecScript.ps1' -Script ")
+	wrapperScript += "\"" + script + "\""
+
+	slog.Debug("Final execution script", "script", wrapperScript)
+
+	return wrapperScript, nil
 }
