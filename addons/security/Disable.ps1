1879fd766926ad701a52e99a43a0e8bc82567fb4
Artur Pusztai <162565584+artur-pusztai@users.noreply.github.com>
2024-04-22 14:18:14 +0200

    cert-manager addon (#358)
    First draft of the security add-on. It installs cert-manager and prepares it to automate TLS certificate provisioning for the basic security mode, in combination with one of the ingress add-ons: ingress-nginx or traefik.

---
+++ addons/security/Disable.ps1
@@ -0,0 +1,77 @@
+# SPDX-FileCopyrightText: © 2023 Siemens Healthcare GmbH
+#
+# SPDX-License-Identifier: MIT
+
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+Uninstalls security
+
+.DESCRIPTION
+
+.EXAMPLE
+Disable security addon
+powershell <installation folder>\addons\security\Disable.ps1
+#>
+
+Param (
+    [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
+    [switch] $ShowLogs = $false,
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType
+)
+&$PSScriptRoot\..\..\smallsetup\common\GlobalVariables.ps1
+. $PSScriptRoot\..\..\smallsetup\common\GlobalFunctions.ps1
+. $PSScriptRoot\Common.ps1
+
+$logModule = "$PSScriptRoot/../../smallsetup/ps-modules/log/log.module.psm1"
+$statusModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.cluster.module/status/status.module.psm1"
+$addonsModule = "$PSScriptRoot\..\addons.module.psm1"
+$infraModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
+
+Import-Module $logModule, $addonsModule, $statusModule, $infraModule
+
+Initialize-Logging -ShowLogs:$ShowLogs
+
+Write-Log 'Checking cluster status' -Console
+
+$systemError = Test-SystemAvailability -Structured
+if ($systemError) {
+    if ($EncodeStructuredOutput -eq $true) {
+        Send-ToCli -MessageType $MessageType -Message @{Error = $systemError }
+        return
+    }
+
+    Write-Log $systemError.Message -Error
+    exit 1
+}
+
+if ($null -eq (&$global:KubectlExe get namespace cert-manager --ignore-not-found) -or (Test-IsAddonEnabled -Name 'security') -ne $true) {
+    $errMsg = "Addon 'security' is already disabled, nothing to do."
+
+    if ($EncodeStructuredOutput -eq $true) {
+        $err = New-Error -Severity Warning -Code (Get-ErrCodeAddonAlreadyDisabled) -Message $errMsg
+        Send-ToCli -MessageType $MessageType -Message @{Error = $err }
+        return
+    }
+    
+    Write-Log $errMsg -Error
+    exit 1
+}
+
+Write-Log 'Uninstalling security' -Console
+$certManagerConfig = Get-CertManagerConfig
+$caIssuerConfig = Get-CAIssuerConfig
+&$global:KubectlExe delete -f $caIssuerConfig
+&$global:KubectlExe delete -f $certManagerConfig
+Remove-AddonFromSetupJson -Name 'security'
+Write-Log 'Uninstallation of security finished' -Console
+
+Write-WarningForUser
+
+if ($EncodeStructuredOutput -eq $true) {
+    Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+}
