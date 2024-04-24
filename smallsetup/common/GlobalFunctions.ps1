013a04f0e9988f600ef9dc24eb4c06bd1a15c421
Jacob Grieger <155653123+jgriegershs@users.noreply.github.com>
2024-04-24 13:47:43 +0200

    #346 refactor(smb-share addon): globals removed; e2e test for addon status added (#395)

--- smallsetup/common/GlobalFunctions.ps1
+++ smallsetup/common/GlobalFunctions.ps1
@@ -2152,57 +2152,6 @@ function Perform-TimeSync {
     }
 }
 
-function Get-RandomPassword {
-    param (
-        [Parameter(Mandatory)]
-        [ValidateRange(4, [int]::MaxValue)]
-        [int] $length,
-        [int] $upper = 1,
-        [int] $lower = 1,
-        [int] $numeric = 1,
-        [int] $special = 1
-    )
-
-    if ($upper + $lower + $numeric + $special -gt $length) {
-        throw 'number of upper/lower/numeric/special char must be lower or equal to length'
-    }
-
-    $uCharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    $lCharSet = 'abcdefghijklmnopqrstuvwxyz'
-    $nCharSet = '0123456789'
-    $sCharSet = '/*-+!?()@:_#'
-    $charSet = ''
-
-    if ($upper -gt 0) { $charSet += $uCharSet }
-    if ($lower -gt 0) { $charSet += $lCharSet }
-    if ($numeric -gt 0) { $charSet += $nCharSet }
-    if ($special -gt 0) { $charSet += $sCharSet }
-
-    $charSet = $charSet.ToCharArray()
-
-    $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
-    $bytes = New-Object byte[]($length)
-    $rng.GetBytes($bytes)
-
-    $result = New-Object char[]($length)
-    for ($i = 0 ; $i -lt $length ; $i++) {
-        $result[$i] = $charSet[$bytes[$i] % $charSet.Length]
-    }
-    $password = (-join $result)
-
-    $valid = $true
-    if ($upper -gt ($password.ToCharArray() | Where-Object { $_ -cin $uCharSet.ToCharArray() }).Count) { $valid = $false }
-    if ($lower -gt ($password.ToCharArray() | Where-Object { $_ -cin $lCharSet.ToCharArray() }).Count) { $valid = $false }
-    if ($numeric -gt ($password.ToCharArray() | Where-Object { $_ -cin $nCharSet.ToCharArray() }).Count) { $valid = $false }
-    if ($special -gt ($password.ToCharArray() | Where-Object { $_ -cin $sCharSet.ToCharArray() }).Count) { $valid = $false }
-
-    if (!$valid) {
-        $password = Get-RandomPassword $length $upper $lower $numeric $special
-    }
-
-    return $password
-}
-
 function Stop-InstallIfNoMandatoryServiceIsRunning {
     $hns = Get-Service 'hns' -ErrorAction SilentlyContinue
     if (!$hns) {
