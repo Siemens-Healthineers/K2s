c75be4e9992e996476da1d17f1fb5a10a2d293c5
Jacob Grieger <jacob.grieger@siemens-healthineers.com>
2024-04-25 09:03:48 +0200

    #346 fix(addons status): kubectl error case now gets captured

--- addons/gpu-node/Get-Status.ps1
+++ addons/gpu-node/Get-Status.ps1
@@ -6,9 +6,9 @@
 
 Import-Module "$PSScriptRoot/../../lib/modules/k2s/k2s.cluster.module/k8s-api/k8s-api.module.psm1"
 
-Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Available', '-n', 'gpu-node', 'deployment/nvidia-device-plugin' | Out-Null
+$success = (Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Available', '-n', 'gpu-node', 'deployment/nvidia-device-plugin').Success
 
-$isDevicePluginRunningProp = @{Name = 'IsDevicePluginRunning'; Value = $?; Okay = $? }
+$isDevicePluginRunningProp = @{Name = 'IsDevicePluginRunning'; Value = $success; Okay = $success }
 if ($isDevicePluginRunningProp.Value -eq $true) {
     $isDevicePluginRunningProp.Message = 'The gpu node is working'
 }
@@ -16,9 +16,9 @@ else {
     $isDevicePluginRunningProp.Message = "The gpu node is not working. Try restarting the cluster with 'k2s start' or disable and re-enable the addon with 'k2s addons disable gpu-node' and 'k2s addons enable gpu-node'"
 } 
 
-Invoke-Kubectl -Params 'rollout', 'status', 'daemonset', 'dcgm-exporter', '-n', 'gpu-node', '--timeout=5s' | Out-Null
+$success = (Invoke-Kubectl -Params 'rollout', 'status', 'daemonset', 'dcgm-exporter', '-n', 'gpu-node', '--timeout=5s').Success
 
-$isDCGMExporterRunningProp = @{Name = 'IsDCGMExporterRunning'; Value = $?; Okay = $? }
+$isDCGMExporterRunningProp = @{Name = 'IsDCGMExporterRunning'; Value = $success; Okay = $success }
 if ($isDCGMExporterRunningProp.Value -eq $true) {
     $isDCGMExporterRunningProp.Message = 'The DCGM exporter is working'
 }
