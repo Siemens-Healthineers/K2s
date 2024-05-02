b22fa0ec974ac74f8b02f905aba603efd4ce7e7b
Jacob Grieger <155653123+jgriegershs@users.noreply.github.com>
2024-05-02 14:59:40 +0200

    #346 refactor(addons): temporary module removed (#436)

--- addons/dashboard/Enable.ps1
+++ addons/dashboard/Enable.ps1
@@ -42,7 +42,7 @@ Param (
 )
 $clusterModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.cluster.module/k2s.cluster.module.psm1"
 $infraModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
-$addonsModule = "$PSScriptRoot\..\addons.v2.module.psm1"
+$addonsModule = "$PSScriptRoot\..\addons.module.psm1"
 $commonModule = "$PSScriptRoot\common.module.psm1"
 
 Import-Module $clusterModule, $infraModule, $addonsModule, $commonModule
