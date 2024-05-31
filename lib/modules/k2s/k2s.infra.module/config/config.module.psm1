9e6b17b762291ca3c2dbd26f8d2e7c77c600a150
dieter.krotz <dieter.krotz@siemens-healthineers.com>
2024-05-31 13:53:11 +0200

    Move to 1.29.5

--- lib/modules/k2s/k2s.infra.module/config/config.module.psm1
+++ lib/modules/k2s/k2s.infra.module/config/config.module.psm1
@@ -446,7 +446,7 @@ function Get-DefaultProvisioningBaseImageDiskSize {
 }
 
 function Get-DefaultK8sVersion {
-    return 'v1.28.10'
+    return 'v1.29.5'
 }
 
 function Get-DefaultCrioVersion {
--- smallsetup/linuxnode/ubuntu/ExistingUbuntuComputerAsMasterNodeInstaller.unit.tests.ps1
+++ smallsetup/linuxnode/ubuntu/ExistingUbuntuComputerAsMasterNodeInstaller.unit.tests.ps1
@@ -90,7 +90,7 @@ Describe 'ExistingUbuntuComputerAsMasterNodeInstaller.ps1' -Tag 'unit', 'ci', 'l
             }
             $expectedNewUserName = 'remote'
             $expectedNewUserPwd = 'admin'
-            $expectedK8sVersion = 'v1.28.10'
+            $expectedK8sVersion = 'v1.29.5'
             $expectedCrioVersion = '1.25.2'
             $expectedDnsIpAddresses = 'myDnsIpAddress1,myDnsIpAddress2'
             $expectedPrefixLength = '24'
