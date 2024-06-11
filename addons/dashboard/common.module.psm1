98ed648994cb8dc01f97e6243f72f426927a32d1
dieter.krotz <dieter.krotz@siemens-healthineers.com>
2024-06-11 15:24:36 +0200

    Adapt URLs in UI apps

--- addons/dashboard/common.module.psm1
+++ addons/dashboard/common.module.psm1
@@ -136,7 +136,7 @@ function Write-UsageForUser {
  eg. k2s addons enable ingress-nginx
  Once the ingress controller is running in the cluster, run the command to enable dashboard again (disable it first if dashboard addon was already enabled).
  k2s addons enable dashboard
- the Kubernetes Dashboard will be accessible on the following URL: https://k2s-dashboard.local
+ the Kubernetes Dashboard will be accessible on the following URL: https://k2s.cluster.local/dashboard/ and https://k2s-dashboard.local (with HTTP using http://.. unstead of https://..)
 
  Option 2: Port-forwarding
  Use port-forwarding to the kubernetes-dashboard using the command below:
