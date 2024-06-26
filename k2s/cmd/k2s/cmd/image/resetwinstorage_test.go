1631639f3f318157fc59b8a5a5be97c4834233b9
Abhitosh <abhitosh@siemens-healthineers.com>
2024-06-26 15:23:01 +0530

    fix: fixed setting context for cobra command.

--- k2s/cmd/k2s/cmd/image/resetwinstorage_test.go
+++ k2s/cmd/k2s/cmd/image/resetwinstorage_test.go
@@ -215,5 +215,5 @@ func resetFlags() {
 	resetWinStorageCmd.Flags().Set(dockerDirFlag, "")
 	resetWinStorageCmd.Flags().Set(maxRetryFlag, "1")
 	resetWinStorageCmd.Flags().Set(forceZapFlag, "false")
-	resetWinStorageCmd.SetContext(context.WithValue(context.TODO(), common.ContextKeyConfig, "some-dir"))
+	resetWinStorageCmd.SetContext(context.WithValue(context.TODO(), common.ContextKeyConfig, &cfg.Config{Host: cfg.HostConfig{K2sConfigDir: "some-dir"}})))
 }
