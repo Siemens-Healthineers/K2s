a980d831057dcbb59a1a4a3f4f6990f0d521c6cd
Mahesh Narayan <154434373+mahenarayan@users.noreply.github.com>
2024-07-04 11:25:38 +0200

    feat(install): distinction of pre-req and install failure (#637)
    Refs: #506

--- k2s/cmd/k2s/cmd/install/core/installer_test.go
+++ k2s/cmd/k2s/cmd/install/core/installer_test.go
@@ -42,10 +42,20 @@ func (m *myMock) Printfln(format string, a ...any) {
 	m.Called(format, a)
 }
 
+func (m *myMock) PrintWarning(a ...any) {
+	m.Called(a)
+}
+
 func (m *myMock) loadConfig(configDir string) (*setupinfo.Config, error) {
 	args := m.Called(configDir)
 
 	return args.Get(0).(*setupinfo.Config), args.Error(1)
+}
+
+func (m *myMock) deleteConfig(configDir string) error {
+	args := m.Called(configDir)
+
+	return args.Error(0)
 }
 
 func (m *myMock) Load(kind ic.Kind, cmdFlags *pflag.FlagSet) (*ic.InstallConfig, error) {
@@ -202,6 +212,7 @@ var _ = Describe("core", func() {
 
 				printerMock := &myMock{}
 				printerMock.On(r.GetFunctionName(printerMock.Printfln), mock.Anything, mock.Anything)
+				printerMock.On(r.GetFunctionName(printerMock.PrintWarning), mock.Anything, mock.Anything)
 
 				configMock := &myMock{}
 				configMock.On(reflection.GetFunctionName(configMock.loadConfig), "some-dir").Return(nilConfig, setupinfo.ErrSystemNotInstalled)
@@ -211,6 +222,8 @@ var _ = Describe("core", func() {
 
 				executorMock := &myMock{}
 				executorMock.On(r.GetFunctionName(executorMock.ExecutePs), testCmd, powershell.PowerShellV5, mock.AnythingOfType("*common.OutputWriter")).Return(expectedError)
+				executorMock.On(r.GetFunctionName(executorMock.ExecutePs), testCmd, powershell.PowerShellV5, mock.AnythingOfType("*common.OutputWriter.ErrorLines")).Return("[PREREQ-FAILED] pre-requisite failed")
+				executorMock.On(r.GetFunctionName(executorMock.ExecutePs), testCmd, powershell.PowerShellV5, mock.AnythingOfType("*common.GetInstallPreRequisiteError")).Return("[PREREQ-FAILED] pre-requisite failed", true)
 
 				sut := &core.Installer{
 					Printer:             printerMock,
@@ -225,6 +238,57 @@ var _ = Describe("core", func() {
 				err := sut.Install(kind, cmd, buildCmdFunc)
 
 				Expect(err).To(MatchError(expectedError))
+			})
+		})
+
+		When("pre-requisites check fails while executing command ", func() {
+			It("returns prints warning", func() {
+				kind := ic.Kind("test-kind")
+				cmd := &cobra.Command{}
+				cmd.SetContext(context.WithValue(context.TODO(), common.ContextKeyConfig, &cfg.Config{Host: cfg.HostConfig{K2sConfigDir: "some-dir"}}))
+
+				config := &ic.InstallConfig{}
+				testCmd := "test-cmd"
+				expectedError := errors.New("oops")
+				var nilConfig *setupinfo.Config
+
+				buildCmdFunc := func(c *ic.InstallConfig) (cmd string, err error) {
+					Expect(c).To(Equal(config))
+					return testCmd, nil
+				}
+
+				printerMock := &myMock{}
+				printerMock.On(r.GetFunctionName(printerMock.Printfln), mock.Anything, mock.Anything)
+				printerMock.On(r.GetFunctionName(printerMock.PrintWarning), mock.Anything, mock.Anything)
+
+				configMock := &myMock{}
+				configMock.On(reflection.GetFunctionName(configMock.loadConfig), "some-dir").Return(nilConfig, setupinfo.ErrSystemNotInstalled)
+				configMock.On(reflection.GetFunctionName(configMock.deleteConfig), "some-dir").Return(nil)
+
+				installConfigMock := &myMock{}
+				installConfigMock.On(r.GetFunctionName(installConfigMock.Load), kind, cmd.Flags()).Return(config, nil)
+
+				prereqErrorLine := "[PREREQ-FAILED] random check fails"
+				executorMock := &myMock{}
+				executorMock.On(r.GetFunctionName(executorMock.ExecutePs), testCmd, powershell.PowerShellV5, mock.AnythingOfType("*common.OutputWriter")).Return(expectedError).Run(func(args mock.Arguments) {
+					ow := args.Get(2).(*common.OutputWriter)
+					ow.WriteErr(prereqErrorLine)
+				})
+
+				sut := &core.Installer{
+					Printer:             printerMock,
+					InstallConfigAccess: installConfigMock,
+					ExecutePsScript:     executorMock.ExecutePs,
+					GetVersionFunc:      func() version.Version { return version.Version{} },
+					GetPlatformFunc:     func() string { return "test-os" },
+					GetInstallDirFunc:   func() string { return "test-dir" },
+					LoadConfigFunc:      configMock.loadConfig,
+					DeleteConfigFunc:    configMock.deleteConfig,
+				}
+
+				err := sut.Install(kind, cmd, buildCmdFunc)
+
+				Expect(err).To(BeNil())
 			})
 		})
 
