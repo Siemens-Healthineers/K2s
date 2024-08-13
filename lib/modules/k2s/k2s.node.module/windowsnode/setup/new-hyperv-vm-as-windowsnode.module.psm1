e455cbbd5917349d980a514d136831e71dbb8039
Gabriel Rosso <154501955+gabriel-rosso@users.noreply.github.com>
2024-08-13 17:13:39 +0200

    refactor(install): MultiVM and LinuxOnly installation types done with modules (#689)

---
+++ lib/modules/k2s/k2s.node.module/windowsnode/setup/new-hyperv-vm-as-windowsnode.module.psm1
@@ -0,0 +1,129 @@
+# SPDX-FileCopyrightText: © 2023 Siemens Healthcare GmbH
+# SPDX-License-Identifier: MIT
+
+#Requires -RunAsAdministrator
+
+$infraModule =   "$PSScriptRoot\..\..\..\k2s.infra.module\k2s.infra.module.psm1"
+$clusterModule = "$PSScriptRoot\..\..\..\k2s.cluster.module\k2s.cluster.module.psm1"
+
+Import-Module $infraModule, $clusterModule
+
+function New-WindowsVmForWorkerNode {
+    Param(
+        [parameter(Mandatory = $false, HelpMessage = 'Windows hostname')]
+        [string] $Hostname = $(throw 'Argument missing: Hostname'),
+        [string]$VmName = $(throw 'Argument missing: VmName'),
+        [parameter(Mandatory = $false, HelpMessage = 'Windows Image to use')]
+        [string] $Image = $(throw 'Argument missing: Image'),
+        [parameter(Mandatory = $false, HelpMessage = 'Startup Memory Size of Windows VM')]
+        [long] $VMStartUpMemory = $(throw 'Argument missing: VMStartUpMemory'),
+        [parameter(Mandatory = $false, HelpMessage = 'Virtual hard disk size of VM')]
+        [long] $VMDiskSize = $(throw 'Argument missing: VMDiskSize'),
+        [parameter(Mandatory = $false, HelpMessage = 'Number of Virtual Processors of Windows VM')]
+        [long] $VMProcessorCount = $(throw 'Argument missing: VMProcessorCount'),
+        [parameter(HelpMessage = 'DNS Addresses')]
+        [string] $DnsAddresses = $(throw 'Argument missing: DnsAddresses'),
+        [parameter(Mandatory = $false, HelpMessage = 'HTTP proxy if available')]
+        [string] $Proxy,
+        [parameter(Mandatory = $false, HelpMessage = 'Deletes the needed files to perform an offline installation')]
+        [switch] $DeleteFilesForOfflineInstallation = $false,
+        [parameter(Mandatory = $false, HelpMessage = 'Force the installation online. This option is needed if the files for an offline installation are available but you want to recreate them.')]
+        [switch] $ForceOnlineInstallation = $false
+    )
+    $provisionedFileName = "Windows-Kubenode-Base.vhdx"
+    $outputPath = "$(Get-KubeBinPath)\$provisionedFileName"
+
+    $isWindowsKubeworkerBaseImageAlreadyAvailable = (Test-Path $outputPath)
+    $isOnlineInstallation = (!$isWindowsKubeworkerBaseImageAlreadyAvailable -or $ForceOnlineInstallation)
+
+    if ($isOnlineInstallation -and $isWindowsKubeworkerBaseImageAlreadyAvailable) {
+        Remove-Item -Path $outputPath -Force
+    }
+    
+    $WSL = Get-ConfigWslFlag
+    $switchname = ''
+    if ($WSL) {
+        $switchname = Get-WslSwitchName
+    }
+    else {
+        $switchname = Get-ControlPlaneNodeDefaultSwitchName
+    }
+   
+    $baseImageCreationParams = @{
+        Hostname = $Hostname
+        VmName = $VmName
+        Image = $Image
+        WinVMStartUpMemory = $VMStartUpMemory
+        VMDiskSize = $VMDiskSize
+        WinVMProcessorCount = $VMProcessorCount
+        DnsAddresses = $DnsAddresses
+        SwitchName = $switchname
+        Proxy = $Proxy
+        Generation = 2
+        Edition = 'Windows 10 Pro'
+        Locale = 'en-US'
+        OutputPath = $outputPath
+    }
+    New-ProvisionedWindowsNodeBaseImage @baseImageCreationParams
+   
+
+    $vmmsSettings = Get-CimInstance -namespace root\virtualization\v2 Msvm_VirtualSystemManagementServiceSettingData
+    $vhdxPath = Join-Path $vmmsSettings.DefaultVirtualHardDiskPath "$VmName.vhdx"
+    Write-Log "Remove '$vhdxPath' if existing"
+    if (Test-Path $vhdxPath) {
+        Remove-Item $vhdxPath -Force
+    }
+    Copy-Item -Path $outputPath -Destination $vhdxPath -Force
+
+    if ($DeleteFilesForOfflineInstallation) {
+        Remove-Item -Path $outputPath -Force
+    }
+
+    $vmCreationParams = @{
+        VmName = $VmName
+        VhdxPath = $vhdxPath
+        SwitchName = $switchname
+        VMStartUpMemory = $VMStartUpMemory
+        VMProcessorCount = $VMProcessorCount
+        Generation = 2
+    }
+
+    New-VmFromImage @vmCreationParams
+}
+
+function New-VmFromImage {
+    Param(
+        [string]$VmName = $(throw "Argument missing: VmName"),
+        [parameter(Mandatory = $false, HelpMessage = 'Windows Image to use')]
+        [string] $VhdxPath,
+        [parameter(Mandatory = $false, HelpMessage = 'Windows Image to use')]
+        [string] $SwitchName,
+        [parameter(Mandatory = $false, HelpMessage = 'Startup Memory Size of Windows VM')]
+        [long] $VMStartUpMemory = 4GB,
+        [parameter(Mandatory = $false, HelpMessage = 'Number of Virtual Processors of Windows VM')]
+        [long] $VMProcessorCount = 4,
+        [parameter(Mandatory = $false, HelpMessage = 'Generation of the VM, can be 1 or 2')]
+        [int16] $Generation = $(throw "Argument missing: Generation")
+    )
+    $virtualMachine = New-VM -Name $VmName -Generation $Generation -MemoryStartupBytes $VMStartUpMemory -VHDPath $VhdxPath -SwitchName $SwitchName
+
+    $virtualMachine | Set-VMProcessor -Count $VMProcessorCount
+    $virtualMachine | Set-VMMemory -DynamicMemoryEnabled:$false
+
+    $virtualMachine | Get-VMIntegrationService | Where-Object { $_ -is [Microsoft.HyperV.PowerShell.GuestServiceInterfaceComponent] } | Enable-VMIntegrationService -Passthru
+
+    $command = Get-Command Set-VM
+    if ($command.Parameters.AutomaticCheckpointsEnabled) {
+        # We need to disable automatic checkpoints
+        $virtualMachine | Set-VM -AutomaticCheckpointsEnabled $false
+    }
+
+    #Start VM and wait for heartbeat
+    $virtualMachine | Start-VM
+
+    Write-Log 'Waiting for VM Heartbeat...'
+    Wait-VM -Name $VmName -For Heartbeat
+}
+
+Export-ModuleMember -Function New-WindowsVmForWorkerNode
+
--- smallsetup/multivm/Uninstall_MultiVMK8sSetup.ps1
+++ smallsetup/multivm/Uninstall_MultiVMK8sSetup.ps1
@@ -44,100 +44,11 @@ Param(
     [switch] $DeleteFilesForOfflineInstallation = $false
 )
 
-
-&$PSScriptRoot\..\common\GlobalVariables.ps1
-. $PSScriptRoot\..\common\GlobalFunctions.ps1
-Import-Module "$PSScriptRoot/../../addons/addons.module.psm1"
-Import-Module "$PSScriptRoot/../ps-modules/log/log.module.psm1"
-Import-Module "$PSScriptRoot\..\..\lib\modules\k2s\k2s.node.module\k2s.node.module.psm1"
-Initialize-Logging -ShowLogs:$ShowLogs
-
-$ErrorActionPreference = 'Continue'
-
-if ($Trace) {
-    Set-PSDebug -Trace 1
+$uninstallParameters = @{
+    SkipPurge = $SkipPurge
+    ShowLogs = $ShowLogs
+    AdditionalHooksDir = $AdditionalHooksDir
+    DeleteFilesForOfflineInstallation = $DeleteFilesForOfflineInstallation
 }
 
-Write-Log '---------------------------------------------------------------'
-Write-Log 'Multi-VM Kubernetes Deinstallation started.'
-Write-Log '---------------------------------------------------------------'
-
-if (! $SkipPurge) {
-    # this negative logic is important to have the right defaults:
-    # if UninstallK8s is called directly, the default is to purge
-    # if UninstallK8s is called from InstallK8s, the default is not to purge
-    $global:PurgeOnUninstall = $true
-}
-
-$linuxOnly = Get-LinuxOnlyFromConfig
-
-Write-Log 'First stop complete K8s incl. VMs'
-& "$global:KubernetesPath\smallsetup\multivm\Stop_MultiVMK8sSetup.ps1" -AdditionalHooksDir:$AdditionalHooksDir -StopDuringUninstall
-
-if ($linuxOnly -ne $true) {
-    Stop-VirtualMachine $global:MultiVMWindowsVMName
-
-    # remove from kubeswitch
-    $svm = Get-VMNetworkAdapter -VMName $global:MultiVMWindowsVMName -ErrorAction SilentlyContinue
-    if ( $svm ) {
-        Write-Log "VM with name: $global:MultiVMWindowsVMName found"
-        Write-Log "Disconnect current network adapter from VM: $global:MultiVMWindowsVMName"
-
-        Disconnect-VMNetworkAdapter -VMName $global:MultiVMWindowsVMName
-    }
-
-    Write-Log "Removing $global:MultiVMWindowsVMName VM" -Console
-    Remove-VirtualMachine $global:MultiVMWindowsVMName
-}
-
-Write-Log "Removing $global:VMName VM" -Console
-& $PSScriptRoot\..\kubemaster\UninstallKubeMaster.ps1 -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
-Remove-KubeNodeBaseImage -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
-
-Write-Log 'Cleaning up' -Console
-Write-Log 'Remove previous VM key from known_hosts file'
-ssh-keygen.exe -R $global:IP_Master 2>&1 | % { "$_" }
-ssh-keygen.exe -R $global:MultiVMWinNodeIP 2>&1 | % { "$_" }
-
-Invoke-AddonsHooks -HookType 'AfterUninstall'
-
-# TODO: necessary on host-only?
-# remove folders from installation drive
-Get-ChildItem -Path $global:KubeletConfigDir -Force -Recurse -Attributes Reparsepoint -ErrorAction 'silentlycontinue' | ForEach-Object { $n = $_.FullName.Trim('\'); fsutil reparsepoint delete "$n" }
-Remove-Item -Path "$($global:SystemDriveLetter):\etc" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item -Path "$($global:SystemDriveLetter):\run" -Force -Recurse -ErrorAction SilentlyContinue
-
-if ($global:PurgeOnUninstall) {
-    Remove-Item -Path "$global:KubernetesPath\smallsetup\multivm\debian*.qcow2" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:K2sConfigDir" -Force -Recurse -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesImagesJson" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:BinPath\kube*.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:BinPath\cri*.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:BinPath\crictl.yaml" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:ExecutableFolderPath" -Force -Recurse -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\config" -Force -ErrorAction SilentlyContinue
-
-    #Backward compatibility for few versions
-    Remove-Item -Path "$global:KubernetesPath\cni\bin\win*.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni\bin\flannel.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni\bin\host-local.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni\bin\vfprules.json" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni\bin" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni\conf" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\cni" -Force -ErrorAction SilentlyContinue
-
-    Remove-Item -Path "$global:KubernetesPath\bin\cni\win*.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\bin\cni\flannel.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\bin\cni\host-local.exe" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\bin\cni\vfprules.json" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "$global:KubernetesPath\kubevirt\bin\*.exe" -Force -ErrorAction SilentlyContinue
-
-    Remove-Item -Path ($global:SshConfigDir + '\kubemaster') -Force -Recurse -ErrorAction SilentlyContinue
-    Remove-Item -Path ($global:SshConfigDir + '\windowsvm') -Force -Recurse -ErrorAction SilentlyContinue
-}
-
-Reset-EnvVars
-
-Write-Log 'Uninstalling MultiVMK8s setup done.'
-
-Save-Log -RemoveVar
+& "$PSScriptRoot\..\..\lib\scripts\multivm\uninstall\uninstall.ps1" @uninstallParameters
