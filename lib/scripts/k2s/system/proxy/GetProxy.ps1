d564d7ffc485810a289e34350b37cf5265e5786e
Abhitosh <abhitosh@siemens-healthineers.com>
2024-08-16 12:01:33 +0530

    feat: backend ps scripts for proxy commands

---
+++ lib/scripts/k2s/system/proxy/GetProxy.ps1
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: © 2024 Siemens Healthcare GmbH
+#
+# SPDX-License-Identifier: MIT
+
+#Requires -RunAsAdministrator
+
+Param(
+    [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
+    [switch] $ShowLogs = $false,
+
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType
+)
+
+
+$infraModule = "$PSScriptRoot/../../../../modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
+
+Import-Module $infraModule
+
+Initialize-Logging
+
+Write-Log "GetProxy command."
