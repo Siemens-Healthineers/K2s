c8b8247ca2663feb9052329791c99e4055ab14eb
Gabriel Rosso <154501955+gabriel-rosso@users.noreply.github.com>
2024-09-09 10:38:19 +0200

    refactor(install): installation/uninstallation of build-only setup uses modules (#718)

--- lib/scripts/buildonly/uninstall/Uninstall.ps1
+++ lib/scripts/buildonly/uninstall/Uninstall.ps1
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 #Requires -RunAsAdministrator
+
 Param(
     [parameter(Mandatory = $false, HelpMessage = 'Deletes the needed files to perform an offline installation')]
     [switch] $DeleteFilesForOfflineInstallation = $false,
@@ -10,22 +11,33 @@ Param(
     [switch] $ShowLogs = $false
 )
 
-$infraModule = "$PSScriptRoot/../../../modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
-$nodeModule = "$PSScriptRoot/../../../modules/k2s/k2s.node.module/k2s.node.module.psm1"
-Import-Module $infraModule, $nodeModule
+$infraModule = "$PSScriptRoot\..\..\..\modules\k2s\k2s.infra.module\k2s.infra.module.psm1"
+$nodeModule = "$PSScriptRoot\..\..\..\modules\k2s\k2s.node.module\k2s.node.module.psm1"
+$clusterModule = "$PSScriptRoot\..\..\..\modules\k2s\k2s.cluster.module\k2s.cluster.module.psm1"
+
+Import-Module $infraModule, $nodeModule, $clusterModule
 
 Initialize-Logging -ShowLogs:$ShowLogs
 
-Write-Log 'Uninstalling Build Only Environment'
+$ErrorActionPreference = 'Continue'
 
-Uninstall-LinuxNode -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
+# make sure we are at the right place for executing this script
+$installationPath = Get-KubePath
+Set-Location $installationPath
+
+$installationType = 'Build-only'
+Write-Log "Uninstalling $installationType setup"
 
 Uninstall-WinNode
 
-Clear-WinNode -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
+$controlPlaneNodeParams = @{
+    ShowLogs = $ShowLogs
+    DeleteFilesForOfflineInstallation = $DeleteFilesForOfflineInstallation
+    SkipHeaderDisplay = $true
+}
+& "$PSScriptRoot\..\..\control-plane\Uninstall.ps1" @controlPlaneNodeParams
 
-Reset-EnvVars
-
-Write-Log 'Uninstalling Build Only Environment done.'
+Write-Log "K2s $installationType setup uninstalled."
 
 Save-k2sLogDirectory -RemoveVar
+
--- smallsetup/common/UninstallBuildOnlySetup.ps1
+++ smallsetup/common/UninstallBuildOnlySetup.ps1
@@ -10,42 +10,9 @@ Param(
     [switch] $ShowLogs = $false
 )
 
-# load global settings
-&$PSScriptRoot\GlobalVariables.ps1
-# import global functions
-. $PSScriptRoot\GlobalFunctions.ps1
-
-Import-Module "$PSScriptRoot/../ps-modules/log/log.module.psm1"
-Initialize-Logging -ShowLogs:$ShowLogs
-
-Write-Log 'Uninstalling Build Only Environment'
-
-Write-Log 'Uninstall containerd daemon on Windows' -Console
-&"$global:KubernetesPath\smallsetup\windowsnode\UninstallContainerd.ps1"
-
-Write-Log 'Uninstalling docker daemon on Windows' -Console
-& "$global:KubernetesPath\smallsetup\windowsnode\UninstallDockerWin10.ps1"
-
-Write-Log 'Uninstalling httpproxy daemon on Windows' -Console
-Remove-ServiceIfExists 'httpproxy'
-
-Write-Log "Uninstalling $global:VMName" -Console
-& "$global:KubernetesPath\smallsetup\kubemaster\UninstallKubeMaster.ps1" -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
-
-Remove-Item -Path "$global:NssmInstallDirectory\nssm.exe" -Force -ErrorAction SilentlyContinue
-
-Get-ChildItem "$($global:SystemDriveLetter):\var" -Recurse -ErrorAction SilentlyContinue | ? LinkType -eq 'SymbolicLink' | % { $_.Delete() }
-Remove-Item -Path "$global:K2sConfigDir" -Force -Recurse -ErrorAction SilentlyContinue
-
-Write-Log 'Delete downloaded artifacts for the Windows node'
-&"$global:KubernetesPath\smallsetup\windowsnode\downloader\DownloadsCleaner.ps1" -DeleteFilesForOfflineInstallation $DeleteFilesForOfflineInstallation
-
-if (Test-Path($global:DownloadsDirectory)) {
-    Remove-Item $global:DownloadsDirectory -Force -Recurse
+$uninstallationParameters = @{
+    DeleteFilesForOfflineInstallation = $DeleteFilesForOfflineInstallation
+    ShowLogs = $ShowLogs
 }
 
-Reset-EnvVars
-
-Write-Log 'Uninstalling Build Only Environment done.'
-
-Save-Log -RemoveVar
+& "$PSScriptRoot\..\..\lib\scripts\buildonly\uninstall\uninstall.ps1" @uninstallationParameters
