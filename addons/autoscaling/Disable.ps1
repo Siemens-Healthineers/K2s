338cda40736547cf61e6ce2b52450ca664d28dbf
Joachim Kist <joachim.kist@siemens-healthineers.com>
2024-09-11 10:45:05 +0200

    refactor

---
+++ addons/autoscaling/Disable.ps1
@@ -0,0 +1,71 @@
+# SPDX-FileCopyrightText: © 2023 Siemens Healthcare GmbH
+#
+# SPDX-License-Identifier: MIT
+
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+Uninstalls KEDA
+
+.DESCRIPTION
+NA
+
+.EXAMPLE
+# Disable autoscaling in K2s
+powershell <installation folder>\addons\autoscaling\Disable.ps1
+#>
+
+Param (
+    [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
+    [switch] $ShowLogs = $false,
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType
+)
+
+$clusterModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.cluster.module/k2s.cluster.module.psm1"
+$infraModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
+$addonsModule = "$PSScriptRoot\..\addons.module.psm1"
+
+Import-Module $clusterModule, $infraModule, $addonsModule
+
+Initialize-Logging -ShowLogs:$ShowLogs
+
+$kedaManifest = "$PSScriptRoot\manifests\keda-2.15.1.yaml"
+
+Write-Log 'Checking cluster status' -Console
+
+$systemError = Test-SystemAvailability -Structured
+if ($systemError) {
+    if ($EncodeStructuredOutput -eq $true) {
+        Send-ToCli -MessageType $MessageType -Message @{Error = $systemError }
+        return
+    }
+
+    Write-Log $systemError.Message -Error
+    exit 1
+}
+
+if ((Test-IsAddonEnabled -Name 'autoscaling') -ne $true) {
+    $errMsg = "Addon 'autoscaling' is already disabled, nothing to do."
+
+    if ($EncodeStructuredOutput -eq $true) {
+        $err = New-Error -Severity Warning -Code (Get-ErrCodeAddonAlreadyDisabled) -Message $errMsg
+        Send-ToCli -MessageType $MessageType -Message @{Error = $err }
+        return
+    }
+    
+    Write-Log $errMsg -Error
+    exit 1
+}
+
+Write-Log 'Uninstalling KEDA' -Console
+(Invoke-Kubectl -Params 'delete', '-f', $kedaManifest).Output | Write-Log
+Remove-AddonFromSetupJson -Name 'autoscaling'
+Write-Log 'Uninstallation of autoscaling addon finished' -Console
+
+if ($EncodeStructuredOutput -eq $true) {
+    Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+}
---
+++ addons/autoscaling/Enable.ps1
@@ -0,0 +1,89 @@
+# SPDX-FileCopyrightText: © 2023 Siemens Healthcare GmbH
+#
+# SPDX-License-Identifier: MIT
+
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+Installs KEDA
+
+.DESCRIPTION
+NA
+
+.EXAMPLE
+# Enable autoscaling in K2s
+powershell <installation folder>\addons\autoscaling\Enable.ps1
+#>
+
+Param (
+    [parameter(Mandatory = $false, HelpMessage = 'Show all logs in terminal')]
+    [switch] $ShowLogs = $false,
+    [parameter(Mandatory = $false, HelpMessage = 'JSON config object to override preceeding parameters')]
+    [pscustomobject] $Config,
+    [parameter(Mandatory = $false, HelpMessage = 'If set to true, will encode and send result as structured data to the CLI.')]
+    [switch] $EncodeStructuredOutput,
+    [parameter(Mandatory = $false, HelpMessage = 'Message type of the encoded structure; applies only if EncodeStructuredOutput was set to $true')]
+    [string] $MessageType
+)
+$clusterModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.cluster.module/k2s.cluster.module.psm1"
+$infraModule = "$PSScriptRoot/../../lib/modules/k2s/k2s.infra.module/k2s.infra.module.psm1"
+$addonsModule = "$PSScriptRoot\..\addons.module.psm1"
+
+Import-Module $clusterModule, $infraModule, $addonsModule
+
+Initialize-Logging -ShowLogs:$ShowLogs
+
+$kedaManifest = "$PSScriptRoot\manifests\keda-2.15.1.yaml"
+
+Write-Log 'Checking cluster status' -Console
+
+$systemError = Test-SystemAvailability -Structured
+if ($systemError) {
+    if ($EncodeStructuredOutput -eq $true) {
+        Send-ToCli -MessageType $MessageType -Message @{Error = $systemError }
+        return
+    }
+
+    Write-Log $systemError.Message -Error
+    exit 1
+}
+
+if ((Test-IsAddonEnabled -Name 'autoscaling') -eq $true) {
+    $errMsg = "Addon 'autoscaling' is already enabled, nothing to do."
+
+    if ($EncodeStructuredOutput -eq $true) {
+        $err = New-Error -Severity Warning -Code (Get-ErrCodeAddonAlreadyEnabled) -Message $errMsg
+        Send-ToCli -MessageType $MessageType -Message @{Error = $err }
+        return
+    }
+    
+    Write-Log $errMsg -Error
+    exit 1
+}
+
+Write-Log 'Installing KEDA' -Console
+
+(Invoke-Kubectl -Params 'apply', '--server-side','-f', $kedaManifest).Output | Write-Log
+
+$allPodsAreUp = (Wait-ForPodCondition -Condition Ready -Label 'app=keda-operator' -Namespace 'autoscaling' -TimeoutSeconds 120)
+if ($allPodsAreUp -ne $true) {
+    $errMsg = "All KEDA pods could not become ready. Please use kubectl describe for more details.`nInstallation of keda failed."
+    if ($EncodeStructuredOutput -eq $true) {
+        $err = New-Error -Code (Get-ErrCodeAddonEnableFailed) -Message $errMsg
+        Send-ToCli -MessageType $MessageType -Message @{Error = $err }
+        return
+    }
+
+    Write-Log $errMsg -Error
+    exit 1  
+}
+
+Add-AddonToSetupJson -Addon ([pscustomobject] @{Name = 'autoscaling' })
+
+Write-Log 'Installation of autoscaling addon finished.' -Console
+
+if ($EncodeStructuredOutput -eq $true) {
+    Send-ToCli -MessageType $MessageType -Message @{Error = $null }
+}
+
