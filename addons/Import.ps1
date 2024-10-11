055b94583bde45633437f7c3ac79f2e61ae3b132
Joachim Kist <155652202+achimkist@users.noreply.github.com>
2024-10-11 11:55:08 +0200

    fix(addons): use $env:TEMP for extracting addons.zip (#803)

--- addons/Import.ps1
+++ addons/Import.ps1
@@ -42,12 +42,12 @@ if ($systemError) {
 
 Write-Log "Extracting $ZipFile" -Console
 Write-Log '---' -Console
-$dir = Split-Path $ZipFile
-$extractionFolder = "${dir}\tmp-extracted-addons\addons"
+$tmpDir = "$env:TEMP\$(Get-Date -Format ddMMyyyy-HHmmss)-tmp-extracted-addons"
+$extractionFolder = "$tmpDir\addons"
 Remove-Item -Force "${extractionFolder}" -Recurse -Confirm:$False -ErrorAction SilentlyContinue
 # check if drive has enough space to extract the zip file
 $zipSize = (Get-Item $ZipFile).length
-$drive = (Get-Item $dir).PSDrive.Name
+$drive = (Get-Item $env:TEMP).PSDrive.Name
 $freeSpace = (Get-PSDrive -Name $drive).Free
 $freeSpaceGB = [math]::Round($freeSpace / 1GB, 2)
 $zipSizeGB = [math]::Round($zipSize / 1GB, 2)
@@ -64,7 +64,7 @@ if ($zipSize -gt ($freeSpace + $additionalSpace)) {
     Write-Log $errMsg -Error
     exit 1
 }
-Expand-Archive $ZipFile -DestinationPath "$dir\tmp-extracted-addons" -Force
+Expand-Archive $ZipFile -DestinationPath "$tmpDir" -Force
 
 $exportedAddons = (Get-Content "$extractionFolder\addons.json" | Out-String | ConvertFrom-Json).addons
 if ($null -eq $exportedAddons -or $exportedAddons.Count -lt 1) {
@@ -162,7 +162,7 @@ foreach ($addon in $addonsToImport) {
     Write-Log '---' -Console
 }
 
-Remove-Item -Force "${dir}\tmp-extracted-addons" -Recurse -Confirm:$False -ErrorAction SilentlyContinue
+Remove-Item -Force "$tmpDir" -Recurse -Confirm:$False -ErrorAction SilentlyContinue
 
 Write-Log '---'
 Write-Log "Addons '$($addonsToImport.name)' imported successfully!" -Console
