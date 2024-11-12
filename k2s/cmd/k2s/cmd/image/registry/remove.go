387cf5fddd801ef68e7300f8f74f0f6b1d175e53
Jakob Koller <jakob.koller@siemens-healthineers.com>
2024-11-12 15:02:58 +0100

    feat(registry): option to remove registry

---
+++ k2s/cmd/k2s/cmd/image/registry/remove.go
@@ -0,0 +1,111 @@
+// SPDX-FileCopyrightText:  © 2024 Siemens Healthcare AG
+// SPDX-License-Identifier:   MIT
+
+package registry
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/siemens-healthineers/k2s/cmd/k2s/cmd/common"
+
+	"github.com/siemens-healthineers/k2s/cmd/k2s/utils"
+
+	"github.com/siemens-healthineers/k2s/internal/core/setupinfo"
+	"github.com/siemens-healthineers/k2s/internal/powershell"
+
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+)
+
+var (
+	removeExample = `
+	# Remove registry in K2s
+	k2s image registry rm myregistry
+`
+
+	rmCmd = &cobra.Command{
+		Use:     "rm",
+		Short:   "Remove container registry",
+		RunE:    removeRegistry,
+		Example: removeExample,
+	}
+)
+
+func init() {
+	rmCmd.Flags().SortFlags = false
+	rmCmd.Flags().PrintDefaults()
+}
+
+func removeRegistry(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 || args[0] == "" {
+		return errors.New("no registry passed in CLI, use e.g. 'k2s image registry remove <registry-name>'")
+	}
+
+	registryName := args[0]
+
+	slog.Info("Removing registry", "registry", registryName)
+
+	pterm.Printfln("🤖 Removing registry '%s' from K2s cluster", registryName)
+
+	psCmd, params, err := buildRemovePsCmd(registryName, cmd)
+	if err != nil {
+		return err
+	}
+
+	slog.Debug("PS command created", "command", psCmd, "params", params)
+
+	start := time.Now()
+
+	context := cmd.Context().Value(common.ContextKeyCmdContext).(*common.CmdContext)
+	config, err := setupinfo.ReadConfig(context.Config().Host.K2sConfigDir)
+	if err != nil {
+		if errors.Is(err, setupinfo.ErrSystemInCorruptedState) {
+			return common.CreateSystemInCorruptedStateCmdFailure()
+		}
+		if errors.Is(err, setupinfo.ErrSystemNotInstalled) {
+			return common.CreateSystemNotInstalledCmdFailure()
+		}
+		return err
+	}
+
+	if config.SetupName == setupinfo.SetupNameMultiVMK8s {
+		return common.CreateFunctionalityNotAvailableCmdFailure(config.SetupName)
+	}
+
+	cmdResult, err := powershell.ExecutePsWithStructuredResult[*common.CmdResult](psCmd, "CmdResult", common.DeterminePsVersion(config), common.NewPtermWriter(), params...)
+	if err != nil {
+		return err
+	}
+
+	if cmdResult.Failure != nil {
+		return cmdResult.Failure
+	}
+
+	duration := time.Since(start)
+
+	common.PrintCompletedMessage(duration, "image registry rm")
+
+	return nil
+}
+
+func buildRemovePsCmd(registryName string, cmd *cobra.Command) (psCmd string, params []string, err error) {
+	psCmd = utils.FormatScriptFilePath(filepath.Join(utils.InstallDir(), "lib", "scripts", "k2s", "image", "registry", "Remove-Registry.ps1"))
+
+	showOutput, err := strconv.ParseBool(cmd.Flags().Lookup(common.OutputFlagName).Value.String())
+	if err != nil {
+		return "", nil, fmt.Errorf("unable to parse flag '%s': %w", common.OutputFlagName, err)
+	}
+
+	if showOutput {
+		params = append(params, " -ShowLogs")
+	}
+
+	params = append(params, " -RegistryName "+utils.EscapeWithSingleQuotes(registryName))
+
+	return
+}
