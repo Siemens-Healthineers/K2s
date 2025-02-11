c371a86384a63a35c1f071ab4f8422e6837f4736
German Lopez <german.gl.lopez@siemens-healthineers.com>
2025-02-11 13:24:09 +0100

    fix(upgrade): Added additional uTest for #597

--- lib/modules/k2s/k2s.cluster.module/upgrade/upgrade.module.unit.tests.ps1
+++ lib/modules/k2s/k2s.cluster.module/upgrade/upgrade.module.unit.tests.ps1
@@ -24,7 +24,7 @@ Describe 'Assert-UpgradeVersionIsValid' -Tag 'unit', 'ci', 'upgrade' {
         @{ ExpectedResult = $false; Current = '1.0.0'; New = '1.2.0' }
         @{ ExpectedResult = $false; Current = '1.0.0'; New = '2.0.0' }
         @{ ExpectedResult = $false; Current = '2.0.0'; New = '2.2.0' }
-        
+
         @{ ExpectedResult = $false; Current = '1.1.0'; New = '1.0.0' }
         @{ ExpectedResult = $false; Current = '1.1.1'; New = '1.1.0' }
         @{ ExpectedResult = $false; Current = '2.0.0'; New = '1.0.0' }
@@ -87,7 +87,8 @@ Describe 'Invoke-ClusterUninstall' -Tag 'unit', 'ci', 'upgrade' {
         $log = [System.Collections.ArrayList]@()
         Mock -ModuleName $moduleName Copy-Item { }
         Mock -ModuleName $moduleName Write-Log { $log.Add($Messages) | Out-Null }
-        Mock -ModuleName $moduleName Invoke-Cmd { return 0 } 
+        Mock -ModuleName $moduleName Invoke-Cmd { return 0 }
+        Mock -ModuleName $moduleName Test-Path { return $true }
     }
 
     It 'calls Write-Log with correct message' {
@@ -95,9 +96,17 @@ Describe 'Invoke-ClusterUninstall' -Tag 'unit', 'ci', 'upgrade' {
             Invoke-ClusterUninstall
             $log.Count | Should -BeGreaterOrEqual 3
             #In a uTest k2s is not really installed, so the message is correct
-            $log[2] | Should -Be "K2s exe: 'C:\k\k2s.exe' does not exist. Skipping uninstallation."
+            $log[2] | Should -Be 'Uninstall of cluster successfully called'
         }
-    }   
+    }
+
+    It 'calls Invoke-Cmd with correct command' {
+        InModuleScope -ModuleName $moduleName {
+            Invoke-ClusterUninstall
+            Assert-MockCalled Invoke-Cmd -ParameterFilter { $Executable -Match 'k2s.exe' }
+            Should -Invoke -CommandName Invoke-Cmd -Times 1 -Exactly
+        }
+    }
 }
 
 Describe 'Remove-SetupConfigIfExisting' -Tag 'unit', 'ci', 'upgrade' {
@@ -109,29 +118,180 @@ Describe 'Remove-SetupConfigIfExisting' -Tag 'unit', 'ci', 'upgrade' {
 
     Context 'config not existing' {
         BeforeAll {
-            Mock -ModuleName $moduleName Test-Path { return $false } -ParameterFilter { $Path -eq 'my-config' }            
+            Mock -ModuleName $moduleName Test-Path { return $false } -ParameterFilter { $Path -eq 'my-config' }
         }
 
         It 'does nothing' {
             InModuleScope -ModuleName $moduleName {
                 Remove-SetupConfigIfExisting
-                
+
                 Should -Invoke -Scope Context Remove-Item -Times 0
             }
         }
     }
-    
+
     Context 'config existing' {
         BeforeAll {
-            Mock -ModuleName $moduleName Test-Path { return $true } -ParameterFilter { $Path -eq 'my-config' }            
+            Mock -ModuleName $moduleName Test-Path { return $true } -ParameterFilter { $Path -eq 'my-config' }
         }
 
         It 'removes the config' {
             InModuleScope -ModuleName $moduleName {
                 Remove-SetupConfigIfExisting
-                
+
                 Should -Invoke -Scope Context Remove-Item -Times 1 -ParameterFilter { $Path -eq 'my-config' }
             }
+        }
+    }
+}
+
+Describe "Restart-ClusterIfBuildVersionMismatch" {
+    BeforeAll {
+        $log = [System.Collections.ArrayList]@()
+        Mock -ModuleName $moduleName RestartCluster  {  }
+        Mock -ModuleName $moduleName Write-Log { $log.Add($Messages) | Out-Null }
+    }
+
+    It "should restart the cluster if only the build version is different" {
+        InModuleScope -ModuleName $moduleName {
+            $currentVersion = "1.2.3"
+            $nextVersion = "1.2.4"
+            $installFolder = "C:\Program Files\K2s"
+            $kubePath = "C:\Program Files\K2s\kube"
+
+            $result = Restart-ClusterIfBuildVersionMismatch -currentVersion $currentVersion -nextVersion $nextVersion -installFolder $installFolder -kubePath $kubePath
+            # Assert that RestartCluster was called
+            Assert-MockCalled -CommandName RestartCluster -Exactly 1 -Scope It
+
+            # Assert that the function returns $false
+            $result | Should -Be $false
+        }
+    }
+
+    It "should not restart the cluster if the major version is different" {
+        InModuleScope -ModuleName $moduleName {
+            $currentVersion = "1.2.3"
+            $nextVersion = "2.0.0"
+            $installFolder = "C:\Program Files\K2s"
+            $kubePath = "C:\Program Files\K2s\kube"
+
+            $result = Restart-ClusterIfBuildVersionMismatch -currentVersion $currentVersion -nextVersion $nextVersion -installFolder $installFolder -kubePath $kubePath
+
+            # Assert that RestartCluster was not called
+            Assert-MockCalled -CommandName RestartCluster -Exactly 0 -Scope It
+
+            # Assert that the function returns $true
+            $result | Should -Be $true
+        }
+    }
+
+    It "should not restart the cluster if the minor version is increased by more than one" {
+        InModuleScope -ModuleName $moduleName {
+            $currentVersion = "1.2.3"
+            $nextVersion = "1.4.0"
+            $installFolder = "C:\Program Files\K2s"
+            $kubePath = "C:\Program Files\K2s\kube"
+
+            $result = Restart-ClusterIfBuildVersionMismatch -currentVersion $currentVersion -nextVersion $nextVersion -installFolder $installFolder -kubePath $kubePath
+
+            # Assert that RestartCluster was not called
+            Assert-MockCalled -CommandName RestartCluster -Exactly 0 -Scope It
+
+            # Assert that the function returns $true
+            $result | Should -Be $true
+        }
+    }
+
+    It "should not restart the cluster if the minor version is the same and the build version is the same" {
+        InModuleScope -ModuleName $moduleName {
+            $currentVersion = "1.2.3"
+            $nextVersion = "1.2.3"
+            $installFolder = "C:\Program Files\K2s"
+            $kubePath = "C:\Program Files\K2s\kube"
+
+            $result = Restart-ClusterIfBuildVersionMismatch -currentVersion $currentVersion -nextVersion $nextVersion -installFolder $installFolder -kubePath $kubePath
+
+            # Assert that RestartCluster was not called
+            Assert-MockCalled -CommandName RestartCluster -Exactly 0 -Scope It
+
+            # Assert that the function returns $false
+            $result | Should -Be $false
+        }
+    }
+}
+
+Describe "RestartCluster" {
+    BeforeAll {
+        $log = [System.Collections.ArrayList]@()
+        Mock -ModuleName $moduleName Write-Log {
+            $log.Add($Messages) | Out-Null }
+        Mock -ModuleName $moduleName Invoke-Cmd { return 0 }
+        Mock -ModuleName $moduleName Get-SetupInfo { return @{ Name = "k2s" } }
+    }
+
+    It "should not restart the cluster if it is not running" {
+        InModuleScope -ModuleName $moduleName -Parameters @{log = $log } {
+            Mock Get-RunningState { return @{ IsRunning = $false } }
+            RestartCluster -CurrentKubePath "C:\Current\KubePath" -NextVersionKubePath "C:\Next\KubePath"
+            # Assert that Write-Log was called with the expected message
+            Assert-MockCalled -CommandName Write-Log -Exactly 1 -Scope It
+            $log.Count | Should -BeGreaterOrEqual 1
+            $log[0] | Should -Be 'Cluster is not running, no need to restart'
+        }
+    }
+
+    It "should log a message if the k2s.exe does not exist" {
+        InModuleScope -ModuleName $moduleName -Parameters @{log = $log } {
+            Mock Get-RunningState { return @{ IsRunning = $true } }
+            Mock Test-Path  { return $false }
+            RestartCluster -CurrentKubePath "C:\Current\KubePath" -NextVersionKubePath "C:\Next\KubePath"
+            # Assert that Write-Log was called with the expected message
+            Assert-MockCalled -CommandName Write-Log -Exactly 2 -Scope It
+            $log.Count | Should -BeGreaterOrEqual 2
+            $log[2] | Should -Be "K2s exe: 'C:\Current\KubePath\k2s.exe' does not exist. Skipping stop."
+        }
+    }
+
+    It "should restart the cluster if it is running and k2s.exe exists" {
+        InModuleScope -ModuleName $moduleName -Parameters @{log = $log }  {
+            Mock Get-RunningState { return @{ IsRunning = $true } }
+            Mock Test-Path  { return $true }
+            RestartCluster -CurrentKubePath "C:\Current\KubePath" -NextVersionKubePath "C:\Next\KubePath"
+            # Assert that Invoke-Cmd was called with the expected arguments
+            Assert-MockCalled -CommandName Invoke-Cmd -Exactly 1 -Scope It -ParameterFilter { $Executable -eq "C:\Current\KubePath\k2s.exe" -and $Arguments -eq "stop" }
+
+            # Assert that Write-Log was called with the expected message
+            Assert-MockCalled -CommandName Write-Log -Exactly 3 -Scope It
+            $log.Count | Should -BeGreaterOrEqual 5
+            $log[4] | Should -Be "Stop of cluster successfully called"
+            $log[5] | Should -Be "Start of cluster successfully called"
+        }
+    }
+
+    It "should throw an exception if stopping the cluster fails" {
+        InModuleScope -ModuleName $moduleName  -Parameters @{log = $log } {
+            Mock Get-RunningState { return @{ IsRunning = $true } }
+            Mock Test-Path  { return $true }
+            Mock Invoke-Cmd { return 1 }
+            { RestartCluster -CurrentKubePath "C:\Current\KubePath" -NextVersionKubePath "C:\Next\KubePath" } | Should -Throw -ExpectedMessage 'Error: Not possible to stop existing cluster!'
+        }
+    }
+
+    It "should throw an exception if starting the cluster fails" {
+        InModuleScope -ModuleName $moduleName  -Parameters @{log = $log } {
+            Mock Get-RunningState { return @{ IsRunning = $true } }
+            $script:count = 0
+            Mock Test-Path  { return $true }
+            Mock Invoke-Cmd {
+                # This is done to simulate that the first call to Invoke-Cmd is successful and the second fails
+                $script:count++
+                if ($script:count++ -eq 1) {
+                    return 0
+                } else {
+                    return 1
+                }
+             }
+            { RestartCluster -CurrentKubePath "C:\Current\KubePath" -NextVersionKubePath "C:\Next\KubePath" } | Should -Throw -ExpectedMessage 'Error: Not possible to start cluster!'
         }
     }
 }
