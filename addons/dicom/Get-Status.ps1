f87e75aeee296d977dc67872e567fa0310372abc
German Lopez <78157578+GermanGLLopez@users.noreply.github.com>
2025-02-20 18:30:02 +0100

    feat(addons): #802 fix viewer usage with traefik (#998)
    Co-authored-by: dieter.krotz <dieter.krotz@siemens-healthineers.com>

--- addons/dicom/Get-Status.ps1
+++ addons/dicom/Get-Status.ps1
@@ -10,20 +10,30 @@ $success = (Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Avai
 
 $isOrthancRunningProp = @{Name = 'dicom'; Value = $success; Okay = $success }
 if ($isOrthancRunningProp.Value -eq $true) {
-    $isOrthancRunningProp.Message = "The dicom Deployment is working"
+    $isOrthancRunningProp.Message = 'The dicom Deployment is working'
 }
 else {
     $isOrthancRunningProp.Message = "The dicom Deployment is not working. Try restarting the cluster with 'k2s start' or disable and re-enable the addon with 'k2s addons disable dicom' and 'k2s addons enable dicom'"
 } 
 
-$success = (Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Available', '-n', 'dicom', 'deployment/mysql').Success
+# $success = (Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Available', '-n', 'dicom', 'deployment/mysql').Success
 
-$isMySQlRunningProp = @{Name = 'mysql'; Value = $success; Okay = $success }
-if ($isMySQlRunningProp.Value -eq $true) {
-    $isMySQlRunningProp.Message = "The mysql Deployment is working"
+# $isDBSQlRunningProp = @{Name = 'mysql'; Value = $success; Okay = $success }
+# if ($isDBSQlRunningProp.Value -eq $true) {
+#     $isDBSQlRunningProp.Message = "The mysql Deployment is working"
+# }
+# else {
+#     $isDBSQlRunningProp.Message = "The mysql Deployment is not working. Try restarting the cluster with 'k2s start' or disable and re-enable the addon with 'k2s addons disable dicom' and 'k2s addons enable dicom'"
+# } 
+
+$success = (Invoke-Kubectl -Params 'wait', '--timeout=5s', '--for=condition=Available', '-n', 'dicom', 'deployment/postgres').Success
+
+$isDBSQlRunningProp = @{Name = 'postgres'; Value = $success; Okay = $success }
+if ($isDBSQlRunningProp.Value -eq $true) {
+    $isDBSQlRunningProp.Message = 'The postgres Deployment is working'
 }
 else {
-    $isMySQlRunningProp.Message = "The mysql Deployment is not working. Try restarting the cluster with 'k2s start' or disable and re-enable the addon with 'k2s addons disable dicom' and 'k2s addons enable dicom'"
+    $isDBSQlRunningProp.Message = "The postgres Deployment is not working. Try restarting the cluster with 'k2s start' or disable and re-enable the addon with 'k2s addons disable dicom' and 'k2s addons enable dicom'"
 } 
 
-return $isOrthancRunningProp, $isMySQlRunningProp
+return $isOrthancRunningProp, $isDBSQlRunningProp
