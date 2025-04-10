d6bc42a976fb78bb7dc813ac7464c1cee78aeb03
krotz-dieter <dieter.krotz@siemens-healthineers.com>
2025-04-10 16:06:00 +0200

    feat(addons): add in the mesh also traefik and dashboard

--- addons/ingress/nginx/Update.ps1
+++ addons/ingress/nginx/Update.ps1
@@ -14,11 +14,12 @@ Write-Log "Updating addon with name: $addonName"
 $EnancedSecurityEnabled = Test-LinkerdServiceAvailability
 if ($EnancedSecurityEnabled) {
     Write-Log "Updating nginx ingress addon to be part of service mesh"  
-    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"config.linkerd.io/skip-inbound-ports\":\"443,8443\",\"linkerd.io/inject\":\"enabled\"}}}}}'
+    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"config.linkerd.io/skip-inbound-ports\":\"443,8443,10254\",\"config.linkerd.io/skip-outbound-ports\":\"443\",\"linkerd.io/inject\":\"enabled\"}}}}}'
     (Invoke-Kubectl -Params 'patch', 'deployment', 'ingress-nginx-controller', '-n', 'ingress-nginx', '-p', $annotations).Output | Write-Log
 } else {
     Write-Log "Updating nginx ingress addon to not be part of service mesh"
-    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"config.linkerd.io/skip-inbound-ports\":null,\"linkerd.io/inject\":null}}}}}'
+    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"config.linkerd.io/skip-inbound-ports\":null,\"config.linkerd.io/skip-outbound-ports\":null,\"linkerd.io/inject\":null}}}}}'
     (Invoke-Kubectl -Params 'patch', 'deployment', 'ingress-nginx-controller', '-n', 'ingress-nginx', '-p', $annotations).Output | Write-Log
 }
-
+(Invoke-Kubectl -Params 'rollout', 'status', 'deployment', '-n', 'ingress-nginx', '--timeout', '60s').Output | Write-Log
+Write-Log 'Updating ingress nginx addon finished.' -Console
