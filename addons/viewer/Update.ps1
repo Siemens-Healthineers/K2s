49e251bc3583018272960ff04095ccbf92abca6e
krotz-dieter <dieter.krotz@siemens-healthineers.com>
2025-04-25 09:53:02 +0200

    feat(addons): enable enhanced security in viewer

--- addons/viewer/Update.ps1
+++ addons/viewer/Update.ps1
@@ -11,3 +11,17 @@ Import-Module $addonsModule, $viewerModule
 
 Update-IngressForAddon -Addon ([pscustomobject] @{Name = 'viewer' })
 Update-ViewerConfigMap
+
+$EnancedSecurityEnabled = Test-LinkerdServiceAvailability
+if ($EnancedSecurityEnabled) {
+    Write-Log "Updating viewer addon to be part of service mesh"  
+    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"linkerd.io/inject\":\"enabled\"}}}}}'
+    (Invoke-Kubectl -Params 'patch', 'deployment', 'viewerwebapp', '-n', 'viewer', '-p', $annotations).Output | Write-Log
+} else {
+    Write-Log "Updating viewer addon to not be part of service mesh"
+    $annotations = '{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"config.linkerd.io/skip-inbound-ports\":null,\"linkerd.io/inject\":null}}}}}'
+    (Invoke-Kubectl -Params 'patch', 'deployment', 'viewerwebapp', '-n', 'viewer', '-p', $annotations).Output | Write-Log
+}
+(Invoke-Kubectl -Params 'rollout', 'status', 'deployment', '-n', 'viewer', '--timeout', '60s').Output | Write-Log
+
+Write-Log 'Updating viewer addon finished.' -Console
