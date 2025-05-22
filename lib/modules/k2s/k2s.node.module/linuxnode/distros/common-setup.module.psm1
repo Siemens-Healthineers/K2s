0dca461cea04dc831be01fd38b69325cdffd58f9
Abhitosh <abhitosh@siemens-healthineers.com>
2025-05-22 21:07:16 +0530

    feat(certificates): #981 renew kubernetes certificates (#1100)

--- lib/modules/k2s/k2s.node.module/linuxnode/distros/common-setup.module.psm1
+++ lib/modules/k2s/k2s.node.module/linuxnode/distros/common-setup.module.psm1
@@ -1192,10 +1192,6 @@ Function Set-UpMasterNode {
     &$executeRemoteCommand "sudo chown $UserName ~/.kube/config"
     &$executeRemoteCommand 'kubectl get nodes'
 
-    Write-Log 'Install custom DNS server'
-    &$executeRemoteCommand 'sudo DEBIAN_FRONTEND=noninteractive apt-get install dnsutils --yes'
-    &$executeRemoteCommand 'sudo DEBIAN_FRONTEND=noninteractive apt-get install dnsmasq --yes'
-
     Write-Log 'Scale down coredns to 1 replicas'
     &$executeRemoteCommand 'kubectl scale deployment coredns -n kube-system --replicas=1'
 
@@ -1538,33 +1534,16 @@ function New-VmImageForControlPlaneNode {
     $vmNetworkInterfaceName = Get-NetworkInterfaceName
 
     $setUpAsMasterNode = {
-        $addToControlPlane = {
-            $supportForWSLParams = @{
-                UserName             = $vmUserName
-                UserPwd              = $vmUserPwd
-                Hostname             = $Hostname
-                IpAddress            = $IpAddress
-                NetworkInterfaceName = $(Get-NetworkInterfaceName)
-                NetworkMask          = '255.255.255.0'
-                GatewayIpAddress     = $GatewayIpAddress
-            }
-            Edit-SupportForWSL @supportForWSLParams
-        }
-
-        $masterNodeParams = @{
-            NodeName             = $Hostname
-            IpAddress            = $IpAddress
+        $supportForWSLParams = @{
             UserName             = $vmUserName
             UserPwd              = $vmUserPwd
-            K8sVersion           = $kubernetesVersion
-            ClusterCIDR          = $(Get-ConfiguredClusterCIDR)
-            ClusterCIDR_Services = $(Get-ConfiguredClusterCIDRServices)
-            KubeDnsServiceIP     = $(Get-ConfiguredKubeDnsServiceIP)
-            IP_NextHop           = $GatewayIpAddress
-            NetworkInterfaceName = $vmNetworkInterfaceName
-            Hook                 = $addToControlPlane
+            Hostname             = $Hostname
+            IpAddress            = $IpAddress
+            NetworkInterfaceName = $(Get-NetworkInterfaceName)
+            NetworkMask          = '255.255.255.0'
+            GatewayIpAddress     = $GatewayIpAddress
         }
-        Set-UpMasterNode @masterNodeParams
+        Edit-SupportForWSL @supportForWSLParams
     }
 
     $kubemasterCreationParams = @{
@@ -2007,4 +1986,5 @@ Get-DirectoryOfKubenodeImagesOnWindowsHost,
 Get-DirectoryOfLinuxNodeArtifactsOnWindowsHost,
 Get-PathOfLinuxNodeArtifactsPackageOnWindowsHost,
 Copy-KubernetesImagesFromControlPlaneNodeToWindowsHost,
-Update-CoreDNSConfigurationviaSSH
+Update-CoreDNSConfigurationviaSSH,
+Set-UpMasterNode
