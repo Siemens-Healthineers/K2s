1f10cc90d09dd5e2ea5cd8cf41a824e4d88ccf60
Krotz Dieter <dieter.krotz@siemens-healthineers.com>
2025-10-17 09:55:15 +0200

    #1317 test case improvements (#1349)

--- lib/modules/k2s/k2s.node.module/windowsnode/network/network.module.psm1
+++ lib/modules/k2s/k2s.node.module/windowsnode/network/network.module.psm1
@@ -174,6 +174,29 @@ function Remove-ExternalSwitch () {
             $hns | Where-Object Name -Like ('*' + $controlPlaneSwitchName + '*') | Remove-HNSNetwork -ErrorAction SilentlyContinue
         } -ArgumentList @($hns, $controlPlaneSwitchName)
     }
+
+    # check l2BridgeSwitchName still exists and if so remove it, do it with 3 tries
+    Write-Log 'Check l2BridgeSwitchName still exists and if so remove it, do it with 3 tries'
+    $iteration = 3
+    while ($iteration -gt 0) {
+        $iteration--
+        $l2BridgeSwitchName = Get-L2BridgeSwitchName
+        $found = Invoke-HNSCommand -Command { 
+            param($l2BridgeSwitchName)
+            Get-HNSNetwork | Where-Object Name -Like $l2BridgeSwitchName 
+        } -ArgumentList $l2BridgeSwitchName
+        if ($found) {
+            Write-Log "$iteration L2 bridge network switch name: $l2BridgeSwitchName still exists"
+            Invoke-HNSCommand -Command { 
+                param($l2BridgeSwitchName)
+                Get-HNSNetwork | Where-Object Name -Like $l2BridgeSwitchName | Remove-HNSNetwork -ErrorAction SilentlyContinue
+            } -ArgumentList $l2BridgeSwitchName
+        }
+        else {
+            Write-Log "L2 bridge network switch name: $l2BridgeSwitchName removed"
+            break
+        }
+    }
 }
 
 function Set-InterfacePrivate {
