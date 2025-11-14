510bc298c6e8e45cae034dfe2b1aae980b2c1f61
Shashank Wagle K <shashankwagle.k@siemens-healthineers.com>
2025-11-14 16:50:57 +0530

    Fixes #1310 Additional Images in addons manifest now get picked from deployment y… (#1366)
    Co-authored-by: Krotz Dieter <dieter.krotz@siemens-healthineers.com>

--- k2s/test/e2e/addons/export_import_test.go
+++ k2s/test/e2e/addons/export_import_test.go
@@ -174,7 +174,8 @@ var _ = Describe("export and import all addons and make sure all artifacts are a
 
 					exportedImages, err := sos.GetFilesMatch(addonExportDir, "*.tar")
 					Expect(err).ToNot(HaveOccurred())
-					Expect(len(exportedImages)).To(Equal(len(images)))
+					Expect(len(exportedImages)).To(Equal(len(images)),
+						"Expected %d tar files to match %d images", len(exportedImages), len(images))
 
 					_, err = os.Stat(filepath.Join(addonExportDir, "version.info"))
 					Expect(os.IsNotExist(err)).To(BeFalse(), "version.info should exist for addon %s", expectedDirName)
