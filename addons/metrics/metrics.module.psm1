7276fc870e674c61378f3fd61c5f71a80396d7d2
Shashank Wagle K <shashankwagle.k@siemens-healthineers.com>
2025-11-18 20:40:10 +0530

    Moved Windows-exporter from core installation to on demand enabling for monitoring and metrics addon

--- addons/metrics/metrics.module.psm1
+++ addons/metrics/metrics.module.psm1
@@ -12,3 +12,11 @@ Contains common methods for installing and uninstalling Metrics server for Kuber
 function Get-MetricsServerConfig {
     return "$PSScriptRoot\manifests\metrics-server.yaml"
 }
+
+<#
+.DESCRIPTION
+Returns the path to the shared Windows Exporter manifests directory.
+#>
+function Get-WindowsExporterManifestDir {
+    return "$PSScriptRoot\..\common\manifests\windows-exporter"
+}
