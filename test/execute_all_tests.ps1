fcb5f3148a836a5cd2f825a4cb3ac193e5d87280
Jacob Grieger <155653123+jgriegershs@users.noreply.github.com>
2025-11-21 12:58:57 +0100

    fix(users-add): admin group identified by SID (#1440)

--- test/execute_all_tests.ps1
+++ test/execute_all_tests.ps1
@@ -83,7 +83,7 @@ if ($ExcludePowershellTests -and $ExcludeGoTests) {
 Import-Module "$PSScriptRoot\test.module.psm1" -Force
 
 $pesterVersion = '5.7.1'
-$ginkgoVersion = '2.26.0'
+$ginkgoVersion = '2.27.2'
 
 # Normalize and resolve repository root directory robustly (handles .. and spaces)
 $rootDir = Join-Path -Path $PSScriptRoot -ChildPath '..'
