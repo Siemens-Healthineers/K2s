552e4f8dc76857032aeeb931f6ba21d8b3377756
Krotz Dieter <dieter.krotz@siemens-healthineers.com>
2025-12-16 13:25:36 +0100

    #1516 refactoring cluster e2e test part 1 (#1517)

--- k2s/test/e2e/cluster/core/core_test.go
+++ k2s/test/e2e/cluster/core/core_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/siemens-healthineers/k2s/test/framework"
 	"github.com/siemens-healthineers/k2s/test/framework/dsl"
+	"github.com/siemens-healthineers/k2s/test/framework/watcher"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -29,6 +30,7 @@ var manifestDir string
 var proxy string
 
 var testFailed = false
+var podWatcher *watcher.PodWatcher
 
 func TestClusterCore(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -53,6 +55,12 @@ var _ = BeforeSuite(func(ctx context.Context) {
 	GinkgoWriter.Println("Using proxy <", proxy, "> for internet access")
 	GinkgoWriter.Println("Deploying workloads to cluster..")
 
+	// Start pod watcher in background
+	podWatcher = watcher.NewPodWatcher(GinkgoWriter, namespace)
+	if err := podWatcher.Start(ctx, suite.Kubectl().Path()); err != nil {
+		GinkgoWriter.Printf("Warning: failed to start pod watcher: %v\n", err)
+	}
+
 	suite.Kubectl().Run(ctx, "apply", "-k", manifestDir)
 
 	GinkgoWriter.Println("Waiting for Deployments to be ready in namespace <", namespace, ">..")
@@ -75,6 +83,10 @@ var _ = BeforeSuite(func(ctx context.Context) {
 })
 
 var _ = AfterSuite(func(ctx context.Context) {
+	// Stop the pod watcher before cleanup
+	if podWatcher != nil {
+		podWatcher.Stop()
+	}
 
 	GinkgoWriter.Println("Status of cluster after test runs...")
 	status := suite.K2sCli().GetStatus(ctx)
