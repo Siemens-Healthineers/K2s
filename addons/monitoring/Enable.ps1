c6f16f56b48c8348694f107356718903c7482e09
VV Krishna Kanth <vvkrishna.kanth@siemens-healthineers.com>
2026-01-13 20:13:56 +0530

    1498 replace plutono by grafana in monitoring addon (#1572)

--- addons/monitoring/Enable.ps1
+++ addons/monitoring/Enable.ps1
@@ -33,6 +33,9 @@ Import-Module $infraModule, $clusterModule, $addonsModule, $monitoringModule
 
 Initialize-Logging -ShowLogs:$ShowLogs
 
+Write-Log "K2s interacts with Grafana (AGPLv3) solely through its standard, public APIs; no AGPL-licensed code is incorporated or modified, and Grafana is deployed as a container. For this integration scenario, a copyleft assessment was performed with the conclusion that AGPLv3 copyleft obligations are not triggered for this specific scenario." -Console
+Write-Log "[Important] The AGPLv3 terms continue to apply to Grafana itself. Users must independently assess whether the AGPLv3 is appropriate for their use case." -Console
+
 Write-Log 'Checking cluster status' -Console
 
 $systemError = Test-SystemAvailability -Structured
